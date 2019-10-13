d756b9016deb94f998868b51fdcabcefded1690d
ilsur <ilsur131@yandex.ru>
2019-10-13 21:09:17 +0300

    Updates

---
+++ precache-manifest.d8690604a56b90167ae81616a27d54e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac742cffca35a3df55449a74887de6b",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "a80ea649007f9642fb68",
+    "url": "/test-react-app/static/css/main.c457c405.chunk.css"
+  },
+  {
+    "revision": "6c0ef85e203dcb658202",
+    "url": "/test-react-app/static/js/2.8b469dca.chunk.js"
+  },
+  {
+    "revision": "a80ea649007f9642fb68",
+    "url": "/test-react-app/static/js/main.f2ccca6b.chunk.js"
+  },
+  {
+    "revision": "6c0cd1c41ecd483c7b51",
+    "url": "/test-react-app/static/js/runtime-main.44ae9e1b.js"
+  },
+  {
+    "revision": "6a070275c307697e5e2024a2704bec58",
+    "url": "/test-react-app/static/media/bookmarkfilled.6a070275.png"
+  }
+]);
